be2d27538c9ed474ccc442649eb6b0995d098ecd
Sethi1998 <sethicaksu@gmail.com>
2023-10-16 12:19:18 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "http://18.118.198.144:8000",
-        port: '',
-        pathname: "",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
